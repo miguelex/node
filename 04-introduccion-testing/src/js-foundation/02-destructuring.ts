74beb2d00b7529979f7ba6c9dcd55b3805828a8c
miguelex <miguelexepg@gmail.com>
2024-01-18 19:40:07 +0100

    Add factory tests

--- 04-introduccion-testing/src/js-foundation/02-destructuring.ts
+++ 04-introduccion-testing/src/js-foundation/02-destructuring.ts
@@ -10,7 +10,7 @@ const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 // console.table({ SHELL, HOMEBREW_PREFIX, npm_lifecycle_script });
 
 
-const characters = ['Flash','Superman', 'Green Lantern', 'Batman'];
+export const characters = ['Flash','Superman', 'Green Lantern', 'Batman'];
 
 const [ , , ,batman ] = characters;
 
